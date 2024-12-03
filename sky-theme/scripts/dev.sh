c41b39c997fa06e69ed59303fd9cb84002ba6a8e
Germán Fernández <703573+germanftorres@users.noreply.github.com>
2024-12-03 09:27:56 +0100

    changes

--- sky-theme/scripts/dev.sh
+++ sky-theme/scripts/dev.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-HUGO_MODULE_WORKSPACE=hugo.work hugo server --logLevel debug --config hugo.toml,hugo.dev.toml --cleanDestinationDir
+HUGO_MODULE_WORKSPACE=hugo.work hugo server --logLevel debug --config hugo.toml,hugo.dev.toml
